f619ba6e742c79cf4ef3a0b99a0e43ca9fe8bab3
Neftaly601 <neftaly601@gmail.com>
2025-04-07 20:51:55 -0600

    Cambio  08:51 07/04/2025

--- app/src/main/java/com/example/kepler201/activities/Carrito/CarritoComprasActivity.java
+++ app/src/main/java/com/example/kepler201/activities/Carrito/CarritoComprasActivity.java
@@ -27,6 +27,8 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kepler201.Adapter.AdaptadorCarrito;
 import com.example.kepler201.ConexionSQLiteHelper;
